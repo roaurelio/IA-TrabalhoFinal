40c12442f1d66b5cac3c1c2a8dc6168151a5ea65
Rosana <ro.aurelio16@gmail.com>
2019-09-21 21:39:54 -0300

    Atualização do código fonte

--- src/CNN.py
+++ src/CNN.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[1]:
 
 
 import numpy as np
 import os
 import cv2
+import random
 import matplotlib.pyplot as plt
 from PIL import Image
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 
 
-# In[35]:
+# In[2]:
 
 
 data_dir = r"C:\Users\Rosana\Documents\DataSets\datas\train"
+data_dir_test = r"C:\Users\Rosana\Documents\DataSets\datas\test"
 categories = ["fundusImage","other"]
+img_size = 128
 
 
-# In[36]:
+# In[3]:
 
 
-img_size = 256
 training_data = []
 
 def create_training_data():
@@ -37,23 +40,16 @@ def create_training_data():
                 new_array = cv2.resize(img_array, (img_size, img_size))
                 training_data.append([new_array, class_num])
             except Exception as e:
+                print(e)
                 pass
 
-create_training_data()                    
+create_training_data()     
 
 
-# In[37]:
+# In[4]:
 
 
-import random
 random.shuffle(training_data)
-
-#print(training_data[0:])
-#plt.imshow(training_data[1], cmap='gray')
-
-
-# In[44]:
-
 
 x_training = []
 y_training = []
@@ -65,29 +61,15 @@ for features, label in training_data:
 x_training = np.array(x_training).reshape(-1, img_size, img_size, 1)
 
 
-# In[6]:
+# In[5]:
 
 
-import pickle
+# Training
 
-pickle_out = open("x.pickle","wb")
-pickle.dump(x_training, pickle_out)
-pickle_out.close()
-
-pickle_out = open("y.pickle","wb")
-pickle.dump(y_training, pickle_out)
-pickle_out.close()
-
-
-# In[32]:
-
-
-pickle_in = open("x.pickle","rb")
-w = pickle.load(pickle_in)
-
-
-# In[39]:
-
+num_epochs = 10
+batch_size = 14
+val_split = 0.1
+adam = keras.optimizers.Adam(lr=0.002)
 
 x_training = x_training/255.0
 
@@ -106,18 +88,11 @@ model.add(Conv2D(32, (3,3), activation="relu"))
 model.add(Conv2D(64, (3,3), activation="relu"))
 model.add(Conv2D(64, (3,3), activation="relu"))
 model.add(Conv2D(128, (3,3), activation="relu"))
+model.add(Dropout(0.2))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(BatchNormalization())
 
 #bloco 3
-model.add(Conv2D(64, (3,3), activation="relu"))
-model.add(Conv2D(128, (3,3), activation="relu"))
-model.add(Conv2D(128, (3,3), activation="relu"))
-model.add(Conv2D(64, (3,3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(BatchNormalization())
-
-#bloco 4
 model.add(Conv2D(64, (3,3), activation="relu"))
 model.add(Conv2D(32, (3,3), activation="relu"))
 model.add(Conv2D(16, (3,3), activation="relu"))
@@ -127,21 +102,81 @@ model.add(BatchNormalization())
 
 model.add(Flatten())
 
-model.add(Dense(units=128, activation='relu'))
-model.add(Dropout(0.2))
+
 model.add(Dense(units=64, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(units=32, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(units=1, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x_training, y_training, batch_size=16, validation_split=0.1, epochs=1, shuffle=True)
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+historico = model.fit(x_training, y_training, batch_size=batch_size, validation_split=val_split, epochs=num_epochs, shuffle=True)
 model.save('trainingCNN.model')
+
+
+# In[6]:
+
+
+test_data = []
+
+def create_test_data():
+    for category in categories:
+        path = os.path.join(data_dir_test, category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (img_size, img_size))
+                test_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+create_test_data()  
+
+
+# In[8]:
+
+
+random.shuffle(test_data)
+
+x_test = []
+y_test = []
+
+for features, label in test_data:
+    x_test.append(features)
+    y_test.append(label)
+    
+x_test = np.array(x_test).reshape(-1, img_size, img_size, 1)
+prediction = model.predict(x_test)
+perda, acuracia = model.evaluate(x_test, y_test)
 
 
 # In[ ]:
 
 
+print('Resultado teste:', np.argmax(prediction[10]))
+print('Número da imagem de teste:', y_test[10])
+plt.imshow(test_data[10][0])
 
+
+# In[9]:
+
+
+plt.plot(historico.history['acc'])
+plt.plot(historico.history['val_acc'])
+plt.title('Acurácia por épocas')
+plt.xlabel('épocas')
+plt.ylabel('acurácia')
+plt.legend(['treino', 'validação'])
+
+
+# In[10]:
+
+
+plt.plot(historico.history['loss'])
+plt.plot(historico.history['val_loss'])
+plt.title('Perda por épocas')
+plt.xlabel('épocas')
+plt.ylabel('perda')
+plt.legend(['treino', 'validação'])
 
---
+++ src/DeepLearning.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pickle
+import numpy as np
+import os
+import cv2
+import random
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+
+
+# In[4]:
+
+
+data_dir = r"C:\Users\Rosana\Documents\DataSets\datas\train"
+data_dir_test = r"C:\Users\Rosana\Documents\DataSets\datas\test"
+categories = ["fundusImage","other"]
+img_size = 128
+
+
+# In[5]:
+
+
+training_data = []
+
+def create_training_data():
+    for category in categories:
+        path = os.path.join(data_dir, category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (img_size, img_size))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                print(e)
+                pass
+
+create_training_data() 
+
+
+# In[6]:
+
+
+random.shuffle(training_data)
+x_training = []
+y_training = []
+
+for features, label in training_data:
+    x_training.append(features)
+    y_training.append(label)
+    
+x_training = np.array(x_training).reshape(-1, img_size, img_size, 1)
+
+
+# In[8]:
+
+
+# Treinamento #
+
+num_epochs = 10
+batch_size = 63
+val_split = 0.1
+adam = keras.optimizers.Adam(lr=0.002)
+
+x_training = x_training/255.0
+
+model = Sequential()
+
+model.add(Flatten(input_shape=x_training.shape[1:]))
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(32, activation="relu"))
+model.add(Dense(16, activation="relu"))
+model.add(Dense(8, activation="relu"))
+model.add(Dense(2, activation="softmax"))
+
+model.compile(optimizer=adam, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+hist = model.fit(x_training, y_training, batch_size=batch_size, validation_split=val_split, epochs=num_epochs, shuffle=True)
+model.save('trainingDL.model')
+
+
+# In[9]:
+
+
+test_data = []
+
+def create_test_data():
+    for category in categories:
+        path = os.path.join(data_dir_test, category)
+        class_num = categories.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (img_size, img_size))
+                test_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+create_test_data()   
+
+
+# In[10]:
+
+
+# Testes
+
+random.shuffle(test_data)
+
+x_test = []
+y_test = []
+
+for features, label in test_data:
+    x_test.append(features)
+    y_test.append(label)
+    
+x_test = np.array(x_test).reshape(-1, img_size, img_size, 1)
+prediction = model.predict(x_test)
+loss, accuracy = model.evaluate(x_test, y_test)
+
+
+# In[15]:
+
+
+print('Resultado teste:', np.argmax(prediction[80]))
+print('Número da imagem de teste:', y_test[80])
+plt.imshow(test_data[80][0])
+
+
+# In[16]:
+
+
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.title('Acurácia por épocas')
+plt.xlabel('épocas')
+plt.ylabel('acurácia')
+plt.legend(['treino', 'validação'])
+
+
+# In[17]:
+
+
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('Perda por épocas')
+plt.xlabel('épocas')
+plt.ylabel('perda')
+plt.legend(['treino', 'validação'])
+
+
+# In[ ]:
+
+
+
+
